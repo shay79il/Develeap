32416bbcb1266cdf743feb0882022acfe1d54e17
Shay Goldstein <shay79il@gmail.com>
2023-06-28 14:24:09 +0300

    init commit

---
+++ app/webserver.py
@@ -0,0 +1,20 @@
+from flask import Flask, request
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def webserver():
+    if request.method == 'GET':
+        return f"""
+          <html>
+          <head>
+              <p>Hello World!!!</p>
+          </head>
+          </html>
+          """
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000, debug=True)
